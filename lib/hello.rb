bd5663dac6b9c5d73b9045490d53880aecd6fc38
derek chen <derek.g.chen@gmail.com>
2018-03-22 17:01:12 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -3,16 +3,15 @@
 # is not hard-coded (FAILED - 3)
 # fails gracefully when a block is not passed in (FAILED - 4)
 
-def hello_t
+def hello_t(array)
   i = 0
- 
+
   while i < array.length
     yield(array[i])
     i = i + 1
   end
- 
+
   array
 end
 
 # call your method here!
-
