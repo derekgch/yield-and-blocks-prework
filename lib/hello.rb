ed7ca6377a38b3f4c4fa91f22557c1f7c32538b7
derek chen <derek.g.chen@gmail.com>
2018-03-22 17:03:24 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,12 +6,12 @@
 def hello_t(array)
   if block_given?
      i = 0
-  
+
      while i < array.length
        yield(array[i])
        i = i + 1
      end
-  
+
      array
    else
      puts "Hey! No block was given!"
@@ -20,7 +20,7 @@ end
 
 # call your method here!
 
-["Tim", "Tom", "Jim"].each do |name|
+hello_t(["Tim", "Tom", "Jim"]) do |name|
   if name.start_with?("T")
     puts "Hi, #{name}"
   end
